45cc6ab71037e166e7e64e9443b461d0d17aabfc
meowpow-png <marin.softic@proton.me>
2026-06-10 18:47:54 +0200

    Create backend application entrypoint

---
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/application/EnterpriseCoffee.java
@@ -0,0 +1,12 @@
+package io.github.meowpowpng.enterprisecoffee.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseCoffee {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseCoffee.class, args);
+    }
+}

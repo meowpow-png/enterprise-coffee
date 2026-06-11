0b44eaa0d2d17838fc42c046fe4534776514f27e
meowpow-png <marin.softic@proton.me>
2026-06-11 11:52:53 +0200

    Create coffee API exceptions

---
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/api/exception/CoffeeOrderProcessingException.java
@@ -0,0 +1,15 @@
+package io.github.meowpowpng.enterprisecoffee.api.exception;
+
+/**
+ * Indicates that a coffee order cannot be processed.
+ */
+public class CoffeeOrderProcessingException extends CoffeeOrderException {
+
+    public CoffeeOrderProcessingException(String message) {
+        super(message);
+    }
+
+    public CoffeeOrderProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

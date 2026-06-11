00ca519ee6d57b561320d0def5c656c6564ef533
meowpow-png <marin.softic@proton.me>
2026-06-11 14:18:56 +0200

    Create dedicate logger for event handler

--- backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/internal/brew/event/CoffeeBrewJobEventHandler.java
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/internal/brew/event/CoffeeBrewJobEventHandler.java
@@ -7,15 +7,12 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Objects;
 
 @Component
 class CoffeeBrewJobEventHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(CoffeeBrewJobEventHandler.class);
+    private static final CoffeeBrewJobEventHandlerLogger log = new CoffeeBrewJobEventHandlerLogger();
 
     private final CoffeeBrewJobRepository repository;
 
@@ -29,12 +26,12 @@ class CoffeeBrewJobEventHandler {
     @EventListener
     public void onStarted(CoffeeBrewJobStartedEvent event) {
         var id = event.job().id().value();
-        log.debug("Started coffee brew job (id={})", id);
+        log.started(id);
         try {
             repository.create(event.job());
         }
         catch (Exception e) {
-            log.error("Failed to persist coffee brew job (id={})", id, e);
+            log.persistenceFailed(id, e);
         }
     }
 
@@ -43,15 +40,12 @@ class CoffeeBrewJobEventHandler {
     @EventListener
     public void onProgressUpdated(CoffeeBrewJobProgressUpdatedEvent event) {
         var id = event.job().id().value();
-        log.debug("Coffee brew job progress updated (id={}, progress={})",
-                id,
-                event.previousProgress() + "->" +event.job().progress()
-        );
+        log.progressUpdated(id, event.previousProgress(), event.job().progress());
         try {
             repository.update(event.job());
         }
         catch (Exception e) {
-            log.error("Failed to persist coffee brew job (id={})", id, e);
+            log.persistenceFailed(id, e);
         }
     }
 
@@ -60,15 +54,12 @@ class CoffeeBrewJobEventHandler {
     @EventListener
     public void onFinished(CoffeeBrewJobFinishedEvent event) {
         var id = event.job().id().value();
-        log.debug("Coffee brew job finished (id={}, progress={})",
-                id,
-                event.job().progress()
-        );
+        log.finished(id, event.job().progress());
         try {
             repository.update(event.job());
         }
         catch (Exception e) {
-            log.error("Failed to persist coffee brew job (id={})", id, e);
+            log.persistenceFailed(id, e);
         }
     }
 }
---
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/internal/brew/event/CoffeeBrewJobEventHandlerLogger.java
@@ -0,0 +1,45 @@
+package io.github.meowpowpng.enterprisecoffee.coffee.internal.brew.event;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+final class CoffeeBrewJobEventHandlerLogger {
+
+    @SuppressWarnings("LoggerInitializedWithForeignClass")
+    private static final Logger log = LoggerFactory.getLogger(CoffeeBrewJobEventHandler.class);
+
+    private enum Event {
+        JOB_STARTED,
+        JOB_PROGRESS_UPDATED,
+        JOB_FINISHED,
+        JOB_PERSISTENCE_FAILED
+    }
+
+    void started(UUID id) {
+        log.debug("event={} id={}", Event.JOB_STARTED, id);
+    }
+
+    void progressUpdated(UUID id, int previousProgress, int currentProgress) {
+        var progress = previousProgress + "->" + currentProgress;
+        debugProgress(Event.JOB_PROGRESS_UPDATED, id, progress);
+    }
+
+    void finished(UUID id, int progress) {
+        debugProgress(Event.JOB_FINISHED, id, progress);
+    }
+
+    void persistenceFailed(UUID id, Exception exception) {
+        log.error("event={} id={} error={}",
+                Event.JOB_PERSISTENCE_FAILED,
+                id,
+                exception.getClass().getSimpleName(),
+                exception
+        );
+    }
+
+    private static void debugProgress(Event event, UUID id, Object progress) {
+        log.debug("event={} id={} progress={}", event, id, progress);
+    }
+}

0149e78c933de6acce5374a84e9e1dca3791d868
meowpow-png <marin.softic@proton.me>
2026-06-12 18:53:01 +0200

    Rename main configuration class

--- backend/src/main/java/io/github/meowpowpng/enterprisecoffee/infra/config/ApplicationConfiguration.java
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/infra/config/ApplicationConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import java.time.Clock;
 
 @Configuration
-public class CoffeeConfiguration {
+public class ApplicationConfiguration {
 
     @Bean
     Clock clock() {

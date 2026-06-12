92d6f1ca5329106585445b55b590d2312c56b670
meowpow-png <marin.softic@proton.me>
2026-06-12 18:34:49 +0200

    Move async configuration classes to infra

--- backend/src/main/java/io/github/meowpowpng/enterprisecoffee/infra/config/AsyncConfiguration.java
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/infra/config/AsyncConfiguration.java
@@ -1,4 +1,4 @@
-package io.github.meowpowpng.enterprisecoffee.coffee.internal.config;
+package io.github.meowpowpng.enterprisecoffee.infra.config;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;

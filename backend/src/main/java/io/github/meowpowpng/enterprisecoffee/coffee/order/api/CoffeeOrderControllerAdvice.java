0041e2d19363fea474331bd129740f765cd1ec71
meowpow-png <marin.softic@proton.me>
2026-06-12 22:18:23 +0200

    Remove javadoc from order controller

--- backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/order/api/CoffeeOrderControllerAdvice.java
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/order/api/CoffeeOrderControllerAdvice.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/**
- * Handles exceptions raised by coffee controllers.
- */
 @RestControllerAdvice(assignableTypes = CoffeeOrderController.class)
 public class CoffeeOrderControllerAdvice {
 

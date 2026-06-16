2b05f49c78df0b0dccb5efe8ddee3aa68f91f07b
meowpow-png <marin.softic@proton.me>
2026-06-16 23:07:10 +0200

    Write controller class documentation

--- backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/machine/api/CoffeeMachineController.java
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/machine/api/CoffeeMachineController.java
@@ -1,5 +1,7 @@
 package io.github.meowpowpng.enterprisecoffee.coffee.machine.api;
 
+import io.github.meowpowpng.enterprisecoffee.coffee.machine.api.exception.CoffeeMachineProtocolException;
+import io.github.meowpowpng.enterprisecoffee.coffee.machine.api.exception.CoffeeMachineUnavailableException;
 import io.github.meowpowpng.enterprisecoffee.common.ApiEndpoints;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +19,27 @@ public class CoffeeMachineController {
         this.client = client;
     }
 
+    /**
+     * Returns the current coffee machine status.
+     *
+     * @return current coffee machine status
+     *
+     * @throws CoffeeMachineUnavailableException if communication with the machine fails
+     * @throws CoffeeMachineProtocolException if the machine returns an invalid response
+     */
     @GetMapping(ApiEndpoints.MACHINE_STATUS)
     MachineStatusResponse status() {
         return new MachineStatusResponse(client.status().name());
     }
 
+    /**
+     * Returns the current coffee brewing progress.
+     *
+     * @return current coffee-brewing progress
+     *
+     * @throws CoffeeMachineUnavailableException if communication with the machine fails
+     * @throws CoffeeMachineProtocolException if the machine returns an invalid response
+     */
     @GetMapping(ApiEndpoints.MACHINE_PROGRESS)
     MachineProgressResponse progress() {
         var progress = client.progress();
--- backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/order/api/CoffeeOrderController.java
+++ backend/src/main/java/io/github/meowpowpng/enterprisecoffee/coffee/order/api/CoffeeOrderController.java
@@ -1,5 +1,8 @@
 package io.github.meowpowpng.enterprisecoffee.coffee.order.api;
 
+import io.github.meowpowpng.enterprisecoffee.coffee.machine.api.exception.CoffeeMachineProtocolException;
+import io.github.meowpowpng.enterprisecoffee.coffee.order.api.exception.CoffeeOrderInvalidException;
+import io.github.meowpowpng.enterprisecoffee.coffee.order.api.exception.CoffeeOrderProcessingException;
 import io.github.meowpowpng.enterprisecoffee.common.ApiEndpoints;
 
 import org.springframework.http.HttpStatus;
@@ -22,6 +25,19 @@ public class CoffeeOrderController {
         this.service = service;
     }
 
+    /**
+     * Places a coffee order.
+     *
+     * @param request coffee order request
+     *
+     * @return coffee order result
+     *
+     * @throws CoffeeOrderInvalidException if the request cannot be
+     * accepted because the specified coffee order is invalid
+     * @throws CoffeeOrderProcessingException if the order cannot be processed
+     * @throws CoffeeMachineProtocolException if the coffee machine returns an invalid response
+     * @throws IllegalStateException if the machine client returned an unexpected result
+     */
     @PostMapping(ApiEndpoints.COFFEE_ORDER)
     @ResponseStatus(HttpStatus.ACCEPTED)
     CoffeeOrderResponse order(@Valid @RequestBody CoffeeOrderRequest request) {

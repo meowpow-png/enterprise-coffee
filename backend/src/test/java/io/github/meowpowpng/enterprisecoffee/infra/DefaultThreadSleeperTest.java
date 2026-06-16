0871550bb1341cb552eecb367f2e55850d55b6c6
meowpow-png <marin.softic@proton.me>
2026-06-16 14:18:01 +0200

    Write thread sleeper interruption unit test

--- backend/src/test/java/io/github/meowpowpng/enterprisecoffee/infra/DefaultThreadSleeperTest.java
+++ backend/src/test/java/io/github/meowpowpng/enterprisecoffee/infra/DefaultThreadSleeperTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class DefaultThreadSleeperTest {
 
@@ -30,5 +30,30 @@ class DefaultThreadSleeperTest {
     void should_CreateInstance_when_IntervalIsPositive() {
         assertThatCode(() -> new DefaultThreadSleeper(Duration.ofMillis(1)))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Should throw IllegalStateException when thread is interrupted")
+    void should_ThrowIllegalStateException_when_ThreadIsInterrupted() {
+
+        var sleeper = new DefaultThreadSleeper(Duration.ofMillis(1));
+        var interruptFlagRestored = new AtomicBoolean();
+        var exceptionThrown = new AtomicBoolean();
+
+        var thread = new Thread(() -> {
+            Thread.currentThread().interrupt();
+            try {
+                sleeper.sleep();
+            }
+            catch (IllegalStateException e) {
+                exceptionThrown.set(true);
+                interruptFlagRestored.set(Thread.currentThread().isInterrupted());
+            }
+        });
+        thread.start();
+
+        assertThatCode(thread::join).doesNotThrowAnyException();
+        assertThat(exceptionThrown.get()).isTrue();
+        assertThat(interruptFlagRestored.get()).isTrue();
     }
 }

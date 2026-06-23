bb10c91825ad412b9188df472e13282a3348feba
meowpow-png <marin.softic@proton.me>
2026-06-23 18:00:03 +0200

    Create integration test profile annotation

---
+++ backend/src/integrationTest/java/io/github/meowpowpng/enterprisecoffee/support/IntegrationTest.java
@@ -0,0 +1,13 @@
+package io.github.meowpowpng.enterprisecoffee.support;
+
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@ActiveProfiles("test")
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IntegrationTest {}

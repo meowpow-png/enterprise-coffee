1161513ebdc1cb212c0a9029a9e9dec9c89c58c4
meowpow-png <marin.softic@proton.me>
2026-06-26 12:32:14 +0200

    Install and configure svgr vite plugin

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+// noinspection JSUnusedGlobalSymbols
+
+declare module "*.svg?react" {
+    import type { FC, SVGProps } from "react";
+
+    const Component: FC<SVGProps<SVGSVGElement>>;
+    export default Component;
+}

73ed82afe46a7419427a1ba963ed00a5066a4f50
meowpow-png <marin.softic@proton.me>
2026-06-26 10:59:26 +0200

    Create main frontend application component

---
+++ frontend/src/coffee/components/Coffee.tsx
@@ -0,0 +1,12 @@
+import ProgressRing from "./ProgressRing";
+
+function Coffee() {
+    return (
+        <ProgressRing
+            progress={72}
+            type="ESPRESSO"
+        />
+    );
+}
+
+export default Coffee;

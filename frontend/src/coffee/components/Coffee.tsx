dbe326cc1ab1e2313efb4aef6a60159a86a2fef8
meowpow-png <marin.softic@proton.me>
2026-06-26 12:35:31 +0200

    Update coffee main component

--- frontend/src/coffee/components/Coffee.tsx
+++ frontend/src/coffee/components/Coffee.tsx
@@ -1,11 +1,17 @@
+import CoffeeSelector from "./CoffeeSelector";
 import ProgressRing from "./ProgressRing";
+
+import styles from "./Coffee.module.css";
 
 function Coffee() {
     return (
-        <ProgressRing
-            progress={72}
-            type="ESPRESSO"
-        />
+        <main className={styles.root}>
+            <ProgressRing
+                progress={72}
+                type="ESPRESSO"
+            />
+            <CoffeeSelector />
+        </main>
     );
 }
 

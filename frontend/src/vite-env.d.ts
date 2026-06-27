014ea371bd754a800cf9cab5282d23223a26081c
meowpow-png <marin.softic@proton.me>
2026-06-27 10:13:46 +0200

    Define backend URL vite variable

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="vite/client" />
 // noinspection JSUnusedGlobalSymbols
 
+// extend Vite's built-in environment
+// variable types with project-specific variables
+interface ImportMetaEnv {
+    readonly VITE_BACKEND_URL: string;
+}
+
+// declare SVG imports as React components
+// when using the ?react import suffix
 declare module "*.svg?react" {
     import type { FC, SVGProps } from "react";
 

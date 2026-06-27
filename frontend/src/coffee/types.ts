4d23b9bdaeea15f0ba2c5a122868bfc49f3e53a8
meowpow-png <marin.softic@proton.me>
2026-06-27 12:24:44 +0200

    Define frontend request and response DTOs

--- frontend/src/coffee/types.ts
+++ frontend/src/coffee/types.ts
@@ -5,10 +5,57 @@ export type CoffeeType =
 
 export type MachineStatus =
     | "READY"
-    | "BREWING";
+    | "BREWING"
+    | "OFFLINE";
 
-export type CoffeeState = {
-    status: MachineStatus;
-    type: CoffeeType | null;
-    progress: number;
-};
+export type CoffeeOrderStatus =
+    | "PENDING"
+    | "ACCEPTED"
+    | "REJECTED"
+    | "INVALID"
+    | "FAILED";
+
+export interface CoffeeOrder {
+    readonly id: string;
+    readonly type: CoffeeType;
+    readonly status: CoffeeOrderStatus;
+    readonly createdAt: string;
+}
+
+/**
+ * Coffee order request submitted by client.
+ */
+export interface CoffeeOrderRequest {
+    type: CoffeeType;
+}
+
+/**
+ * Result of a coffee order request.
+ */
+export interface CoffeeOrderResponse {
+    readonly message: string;
+}
+
+/**
+ * Collection of coffee orders.
+ */
+export interface CoffeeOrdersResponse {
+    readonly orders: readonly CoffeeOrder[];
+}
+
+/**
+ * Current operational status
+ * reported by the coffee machine.
+ */
+export interface MachineStatusResponse {
+    readonly status: MachineStatus;
+}
+
+/**
+ * Current coffee brewing
+ * progress reported by the machine.
+ */
+export interface MachineProgressResponse {
+    readonly type: CoffeeType | "";
+    readonly progress: number;
+}
